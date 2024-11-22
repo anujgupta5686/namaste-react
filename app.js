71bec96c55e909c0f98f7617fecaf647856d987d
anujgupta5686 <anujgupta5686@gmail.com>
2024-11-22 14:52:41 +0530

    Initialize first code

---
+++ app.js
@@ -0,0 +1,26 @@
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>I'm H1 tag</h1>
+        <h1>I'm H1 tag</h1>
+    </div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[
+    React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])
+]);
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello Duniya!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
